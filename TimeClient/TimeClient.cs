f245d462a5ab4d080a237682be8d6b1f0db88c5e
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-05-17 15:54:32 +0200

    Add project files.

---
+++ TimeClient/TimeClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeClient
+{
+    public class TimeClient
+    {
+
+        public void SendRequest()
+        {
+            TcpClient me = null;
+            try
+            {
+                Console.Write("\n\nInsert host name: ");
+                string hostName = Console.ReadLine();
+                Console.Write("\n\nInsert port: ");
+                int port = int.Parse(Console.ReadLine());
+
+                IPAddress addr = null;
+                IPHostEntry ipInfo = Dns.GetHostEntry(hostName);
+                for (int i = 0; i < ipInfo.AddressList.Length; i++)
+                {
+                    if (ipInfo.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        addr = ipInfo.AddressList[i];
+                    }
+                }
+
+                me = new TcpClient();
+                Console.WriteLine("\t\tAwaiting connection");
+                me.Connect(addr, port);
+
+                StreamWriter writer = new StreamWriter(me.GetStream());
+                StreamReader reader = new StreamReader(me.GetStream());
+                writer.AutoFlush = true;
+                Console.Write("\t\tSend a message to the server: ");
+                writer.WriteLine(Console.ReadLine());
+                string res = reader.ReadLine();
+                Console.WriteLine("\t\tServer response: " + res);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                me?.Close();
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            TimeClient c = new TimeClient();
+            c.SendRequest();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TimeServer/TimeServer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeServer
+{
+    public class TimeServer
+    {
+        private IPAddress IpAddress;
+
+        private int Port;
+
+        public TimeServer(int port)
+        {
+            Console.WriteLine("Host name: " + Dns.GetHostName());
+            this.Port = port;
+            try
+            {
+                IPHostEntry ipInfo = Dns.GetHostEntry(Dns.GetHostName());
+                for(int i = 0; i < ipInfo.AddressList.Length; i++)
+                {
+                    if(ipInfo.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        IpAddress = ipInfo.AddressList[i];
+                    }
+                }
+                
+            }
+            catch (Exception) { }
+            
+            
+        }
+
+        public async void Run()
+        {
+            TcpClient client = null;
+            try
+            {
+                while(true)
+                {
+                    TcpListener listener = new TcpListener(IpAddress, Port);
+                    listener.Start();
+                    Console.WriteLine("\t\tAwaiting client");
+                    client = await listener.AcceptTcpClientAsync();
+
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    StreamWriter writer = new StreamWriter(client.GetStream());
+                    writer.AutoFlush = true;
+                    string msg = await reader.ReadLineAsync();
+
+                    string date = DateTime.Now.ToString("dd/MM/yyyy");
+
+                    await writer.WriteLineAsync(date);
+                    Console.WriteLine("\t\tClient satisfied");
+                    client?.Close();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                client?.Close();
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            TimeServer s = new TimeServer(8080);
+            Console.WriteLine("Starting server service");
+            s.Run();
+
+            Console.WriteLine("Method run async");
+            Console.ReadKey();
+        }
+    }
+}
